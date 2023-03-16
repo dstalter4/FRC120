f9e9451e21a5b62c20f1135065c7dc5053ed9258
David Stalter <dstalter4@gmail.com>
2023-03-16 11:41:18 -0400

    First attempt at an auto routine.

--- YtaRobot/src/main/cpp/YtaRobotAutonomous1.cpp
+++ YtaRobot/src/main/cpp/YtaRobotAutonomous1.cpp
@@ -28,6 +28,84 @@
 ////////////////////////////////////////////////////////////////
 void YtaRobot::AutonomousRoutine1()
 {
+    enum StationState
+    {
+        ASCENDING_STATION,
+        BALANCING_STATION,
+        BALANCED
+    };
+    StationState stationState = ASCENDING_STATION;
+
+    // Enable the intake motor to spit out a cube
+    m_pIntakeMotor->Set(ControlMode::PercentOutput, 1.0);
+    Wait(3.0_s);
+    m_pIntakeMotor->Set(ControlMode::PercentOutput, 0.0);
+
+    uint32_t decreasingRollCount = 0U;
+    double lastRoll = 0.0;
+
+    m_pSafetyTimer->Start();
+    units::second_t startTime = m_pSafetyTimer->Get();
+    // @todo: Switch to DriverStation::IsAutonomousEnabled()
+    while ((stationState != BALANCED) && ((m_pSafetyTimer->Get() - startTime) < 10.0_s))
+    {
+        double currentRoll = m_pPigeon->GetRoll();
+
+        switch (stationState)
+        {
+            case ASCENDING_STATION:
+            {
+                m_pSwerveDrive->SetModuleStates({0.20_m, 0.0_m}, 0.0, true, true);
+
+                // If the pitch is > 10 degrees, we can start to try and balance
+                if (currentRoll > 10.0)
+                {
+                    lastRoll = currentRoll;
+                    stationState = BALANCING_STATION;
+
+                    // The delay is because the ramp angle bounces as the robot moves it.
+                    // Give it some time to climb.
+                    AutonomousDelay(0.5_s);
+                }
+
+                break;
+            }
+            case BALANCING_STATION:
+            {
+                if (currentRoll < lastRoll)
+                {
+                    decreasingRollCount++;
+                }
+                else
+                {
+                    decreasingRollCount = 0U;
+                }
+
+                // If we see three consecutive decreasing roll counts, lock wheels and balance
+                if (decreasingRollCount == 3U)
+                {
+                    m_pSwerveDrive->SetModuleStates({0.0_m, 0.0_m}, 1.0, true, true);
+                    AutonomousDelay(0.02_s);
+                    m_pSwerveDrive->SetModuleStates({0.0_m, 0.0_m}, 0.0, true, true);
+                    stationState = BALANCED;
+                }
+
+                lastRoll = currentRoll;
+                AutonomousDelay(0.04_s);
+
+                break;
+            }
+            default:
+            {
+                m_pSwerveDrive->SetModuleStates({0.0_m, 0.0_m}, 0.0, true, true);
+                break;
+            }
+        }
+    }
+    m_pSwerveDrive->SetModuleStates({0.0_m, 0.0_m}, 0.0, true, true);
+    m_pSafetyTimer->Stop();
+    m_pSafetyTimer->Reset();
+
     // Returning from here will enter the idle state until autonomous is over
     RobotUtils::DisplayMessage("Auto routine 1 done.");
 }
